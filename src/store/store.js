f639036fe226f82435f5634db35facafca6ff8b8
onionToyBoy <grazdaninzemlidobri@gmail.com>
2021-11-02 16:41:10 +0300

    Redux devtools has been connected to app

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose} from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import { rootReducer } from './reducers/';
+import { rootReducer } from './reducers';
 import { descriptionSagaWatcher } from './sagas/getDescriptionSaga';
 import { searchSagaWatcher } from './sagas/searchSaga';
 import { showMoreSagaWatcher } from './sagas/showMoreSaga';
 
 export const saga = createSagaMiddleware();
 
-export const store = createStore(rootReducer, applyMiddleware(saga));
+export const store = createStore(rootReducer, compose(applyMiddleware(saga),
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
 
 saga.run(searchSagaWatcher);
 saga.run(showMoreSagaWatcher);
