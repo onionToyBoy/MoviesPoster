e5dc297f3fbb036070afc43fe8e85a079b316d3d
onionToyBoy <grazdaninzemlidobri@gmail.com>
2021-11-02 16:01:02 +0300

    Tests for RadioButton & About components have been created

--- src/components/RadioButton.test.js
+++ src/components/RadioButton.test.js
@@ -5,16 +5,25 @@ import {shallow} from 'enzyme';
 describe('RadioButton test', () => {
     const onClick = jest.fn();
 
-	test('Should render RadioButton component correct', () => {
+	test('Props in RadioButton component should be correct', () => {
         const props= {label:'Test',isSelected: false, fn: onClick}
 		const component = shallow(<RadioButton {...props}/>);
-        RadioButton
+       
         const fn = component.find({ id: 'RadioButton' }).prop('onClick');
         const label = component.find({ id: 'label' }).prop('children');
-        console.log(component.find({ id: 'RadioButton' }).prop('onClick'))
-
+        
         expect(fn).toBe(props.fn);
         expect(label).toBe(props.label);
 		expect(component).toMatchSnapshot();
+    });
+
+    test('Should call fn on user click', () => {
+        const props= {label:'Test',isSelected: false, fn: onClick}
+		const component = shallow(<RadioButton {...props}/>);
+       
+        const button = component.find({ id: 'RadioButton' });
+        button.simulate('click');
+
+        expect(onClick).toHaveBeenCalledWith();
     });
 });
---
+++ src/pages/about/About.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {About} from './About';
+import {shallow} from 'enzyme';
+
+describe('About test', () => {
+
+	test('Should render About component correct', () => {
+		const component = shallow(<About/>);
+		expect(component).toMatchSnapshot();
+    });
+});
